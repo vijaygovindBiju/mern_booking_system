6074015311436e15b920ccff9b82685988e8e2a6
vijaygovindBiju <vijaygovindbiju@gmail.com>
2026-05-31 18:43:24 +0530

    react-hooks issued fixed

--- booking_system/src/Component/AddEvent.jsx
+++ booking_system/src/Component/AddEvent.jsx
@@ -1,10 +1,7 @@
-import { useState, useEffect } from "react";
+import { useState, useEffect, useCallback } from "react";
 import axios from "axios";
-import { useNavigate } from "react-router-dom";
 
 export const AddEvent = ()=>{
-
-    const navigate = useNavigate();
 
     const [event,setEvent] = useState("");
     const [eventsList, setEventsList] = useState([]);
@@ -12,7 +9,7 @@ export const AddEvent = ()=>{
 
     const API_URL = process.env.REACT_APP_API_URL || "http://localhost:8080";
 
-    const fetchData = async () => {
+    const fetchData = useCallback(async () => {
         try {
             const eventsRes = await axios.get(`${API_URL}/events`);
             setEventsList(eventsRes.data);
@@ -22,11 +19,11 @@ export const AddEvent = ()=>{
         } catch (error) {
             console.error("Error fetching admin data:", error);
         }
-    };
+    }, [API_URL]);
 
     useEffect(() => {
         fetchData();
-    }, []);
+    }, [fetchData]);
 
     const addEventHandler =
     async()=>{
